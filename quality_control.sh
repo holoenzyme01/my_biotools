1e3b5f4c6f8577105d98a6751574003343ee69c9
holoenzyme01 <holoenzyme01@gmail.com>
2024-03-08 09:46:38 +0800

    main

--- quality_control.sh
+++ quality_control.sh
@@ -69,7 +69,7 @@ while [ $file_count_2 -ge 0 ]; do
     file_count_2=$((file_count_2 - 1))
     echo ""
     echo "Now we are going to align fastq files: ${r1_files_2[$file_count_2]} and ${r2_files_2[$file_count_2]}."
-    mkdir ../alignment/${r1_files_2[$file_count_2]}/${r2_files_2[$file_count_2]}
+    mkdir -p ../alignment/${r1_files_2[$file_count_2]}/${r2_files_2[$file_count_2]}
     STAR --runMode alignReads --runThreadN 16 --readFilesCommand zcat --twopassMode Basic  --outSAMtype BAM SortedByCoordinate --outSAMunmapped None --genomeDir /data3/lwd/GENOME_REF/FRUIT_FLY/dmel_r6.56/genome_indices --quantMode GeneCounts --readFilesIn ${r1_files_2[$file_count_2]} ${r2_files_2[$file_count_2]} --outFileNamePrefix ../alignment/${r1_files_2[$file_count_2]}/${r2_files_2[$file_count_2]} --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --outFilterType BySJout --alignSJoverhangMin 8 --alignSJDBoverhangMin 1
     wait
 done
