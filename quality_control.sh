5a8567c1e9aad436d551f90af7591e28a21f0d5a
holoenzyme01 <holoenzyme01@gmail.com>
2024-03-08 09:12:51 +0800

    main

--- quality_control.sh
+++ quality_control.sh
@@ -30,7 +30,7 @@ while [ $file_count -ge 0 ]; do
     file_count=$((file_count - 1))
     echo ""
     echo "Now we are going to do the quality control for the fastq files: ${r1_files_1[$file_count]} and ${r2_files_1[$file_count]}."
-    fastp -i ${r1_files_1[$file_count]} -I ${r2_files_1[$file_count]} -o ./fastp_results/${r1_files_1[$file_count]}.fastp.fastq.gz -O ./fastp_results/${r2_files_1[$file_count]}.fastp.fastq.gz -h ./fastp_results/${r1_files_1[$file_count]}.fastp.html -j ./fastp_results/${r1_files_1[$file_count]}.fastp.json
+    fastp -w 16 -i ${r1_files_1[$file_count]} -I ${r2_files_1[$file_count]} -o ./fastp_results/${r1_files_1[$file_count]}.fastp.fastq.gz -O ./fastp_results/${r2_files_1[$file_count]}.fastp.fastq.gz -h ./fastp_results/${r1_files_1[$file_count]}.fastp.html -j ./fastp_results/${r1_files_1[$file_count]}.fastp.json
     wait
 done
 
@@ -64,15 +64,13 @@ for file in $r2_fastq_gz_files_2; do
     r2_files_2+=("$file")
 done
 
-cd ..
-
 file_count_2=${#r1_files_2[@]}
 while [ $file_count_2 -ge 0 ]; do
     file_count_2=$((file_count_2 - 1))
     echo ""
     echo "Now we are going to align fastq files: ${r1_files_2[$file_count_2]} and ${r2_files_2[$file_count_2]}."
-    mkdir ./alignment/${r1_files_2[$file_count_2]}
-    STAR --runMode alignReads --runThreadN 10 --readFilesCommand zcat --twopassMode Basic  --outSAMtype BAM SortedByCoordinate --outSAMunmapped None --genomeDir /data3/lwd/GENOME_REF/FRUIT_FLY/dmel_r6.56/genome_indices --quantMode GeneCounts --readFilesIn ${r1_files_2[$file_count_2]} ${r2_files_2[$file_count_2]} --outFileNamePrefix ./aligment/${r1_files_2[$file_count_2]} --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --outFilterType BySJout --alignSJoverhangMin 8 --alignSJDBoverhangMin 1
+    mkdir ../alignment/${r1_files_2[$file_count_2]}/${r2_files_2[$file_count_2]}
+    STAR --runMode alignReads --runThreadN 16 --readFilesCommand zcat --twopassMode Basic  --outSAMtype BAM SortedByCoordinate --outSAMunmapped None --genomeDir /data3/lwd/GENOME_REF/FRUIT_FLY/dmel_r6.56/genome_indices --quantMode GeneCounts --readFilesIn ${r1_files_2[$file_count_2]} ${r2_files_2[$file_count_2]} --outFileNamePrefix ../alignment/${r1_files_2[$file_count_2]}/${r2_files_2[$file_count_2]} --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --outFilterType BySJout --alignSJoverhangMin 8 --alignSJDBoverhangMin 1
     wait
 done
 
