16a030ef62404170499fd5248514a521c34f2ec5
holoenzyme01 <holoenzyme01@gmail.com>
2024-06-07 11:20:13 +0800

    main

---
+++ CRISPR-KO.R
@@ -0,0 +1,14 @@
+# 基础设置
+folder_path <- read_file("./path.txt")
+count_tables_file_Names <- list.files(folder_path)
+
+# 读取count_table
+count_table <- list()
+for (i in seq_along(count_tables_file_Names)) {
+  count_table[[i]] <- read_csv(count_tables_file_Names[i], col_names = c("spacer","counts"))
+}
+
+#  数据表的合并
+count_table <- reduce(count_table, full_join, by = "spacer")
+cleaned_file_names <- sub("\\.fastq\\.out\\.csv$", "", count_tables_file_Names) # 清理多余的后缀
+colnames(count_table) <- c("spacer", cleaned_file_names)
