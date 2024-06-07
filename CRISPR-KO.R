8dcbe7822f2349606ac1163e0131ab5ed5d15053
holoenzyme01 <holoenzyme01@gmail.com>
2024-06-07 14:39:28 +0800

    main

--- CRISPR-KO.R
+++ CRISPR-KO.R
@@ -12,3 +12,6 @@ for (i in seq_along(count_tables_file_Names)) {
 count_table <- reduce(count_table, full_join, by = "spacer")
 cleaned_file_names <- sub("\\.fastq\\.out\\.csv$", "", count_tables_file_Names) # 清理多余的后缀
 colnames(count_table) <- c("spacer", cleaned_file_names)
+
+# 合并后表格导出
+write_csv(count_table, "./count_table/all_count_table.csv")
